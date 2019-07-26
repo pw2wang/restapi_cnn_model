e8af783aded5a9bf9d4b183790335f84e3547723
Andrew.Li <andrew.li@basebit.ai>
2019-07-26 10:50:28 +0800

    model debug

--- api.py
+++ api.py
@@ -6,42 +6,54 @@ import pandas as pd
 from scipy import misc
 import os
 from conf.config import config
+import cv2
 
 app = Flask(__name__)
 api = Api(app)
 
-
 img_dir = config.img_path
 model_path = config.model_path
+
+classes = ['plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
 def load_model(dir):
     with open(dir,'rb') as f:
         return pickle.load(f)
 
-def load_image_as_input(dir):
-    maxint = 0
-    for filename in os.listdir(img_dir):
-        if filename.endswith(".jpg"):
-            number = int(filename[:-4])
-            if number > maxint:
-                maxint = number
-    image = misc.imread(img_dir+str(maxint)+'.jpg')
+def processing(data):
+    height = 32
+    width = 32
+    data = data[:,:,0:3]
+    dim = (width, height)
+    res = cv2.resize(data, dim, interpolation = cv2.INTER_AREA)
+    return(res)
 
-    return np.reshape(image,(1,32,32,3))
+def load_image_as_input(dir):
+    
+    image = misc.imread(img_dir+'target.jpg')
+    image_process = processing(image)
+
+    return np.reshape(image_process,(1,32,32,3))
 
 # load model and input
+
 model = load_model(model_path)
-X = load_image_as_input(img_dir)
+k = misc.imread('dev/test/test.jpg')
+k = np.reshape(k,(1,32,32,3))
+y = model.predict(k)
 
 # make prediction
-answer = model.predict(X)
 
 
 class HelloWorld(Resource):
     def get(self):
+        X = load_image_as_input(img_dir)
+        answer = model.predict(X)
         y = np.reshape(answer,(10))
         y = np.argmax(y)
-        yy=pd.Series(y).to_json(orient='values')
+        proj_y = classes[y]
+        yy=pd.Series(proj_y).to_json(orient='values')
 
         return yy
 
